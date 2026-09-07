48c8a7401da7fcfa487bd31d0b472cb0b4a12e26
vorga2 <daniil.sedov.2011@gmail.com>
2026-09-08 01:27:45 +0300

    Add pattern step model

---
+++ app/src/main/java/dev/vorga/natromobile/PatternStep.java
@@ -0,0 +1,13 @@
+package dev.vorga.natromobile;
+
+final class PatternStep {
+    final float x;
+    final float y;
+    final double tiles;
+
+    PatternStep(float x, float y, double tiles) {
+        this.x = x;
+        this.y = y;
+        this.tiles = tiles;
+    }
+}
